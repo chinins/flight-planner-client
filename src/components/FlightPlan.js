751ce97481111b3ab29b7de452ca4895511f8290
jayaFeola <34057551+chinins@users.noreply.github.com>
2018-11-21 14:07:46 +0100

    add details on active plan

--- src/components/FlightPlan.js
+++ src/components/FlightPlan.js
@@ -8,10 +8,24 @@ function FlightPlan (props) {
 
   const { name, coordinates} = props.plan;
 
+  const displayCoordinates = () => {
+    return (
+      coordinates.map((coords, index) => (
+        <div>{`Point ${index + 1}: [${coords[0]}, ${coords[1]}]`}</div>
+      ))
+    );
+  };
+
   return (
     <div className={props.ifActive ? 'FlightPlan-active' : 'FlightPlan'} onClick={displayRoute}>
       <div className="plan-name">{name}</div>
       <div className="plan-points">Number of points: {coordinates.length}</div>
+      { props.ifActive &&
+        <div className="plan-coordinates">
+          Coordinates:
+          {displayCoordinates()}
+        </div>
+      }
     </div>
   )
 }
